ba6aff88dd501279e56ff8d2fb6d125aa4ebf915
Joe Hawkins <joe@aktify.com>
2020-02-26 15:30:52 -0700

    feat[ui]: fetch data from API through Apollo Client using apollo-link-rest
    Apollo Boost (apollo-boost) was removed because it does not support providing a link to the ApolloClient constructor

--- services/ui/src/index.js
+++ services/ui/src/index.js
@@ -1,10 +1,23 @@
+import { ApolloProvider } from '@apollo/client';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { RestLink } from 'apollo-link-rest';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+    link: new RestLink({ uri: process.env.REACT_APP_API_URL }),
+    cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App/>
+    </ApolloProvider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- services/ui/src/views/DashboardView/index.js
+++ services/ui/src/views/DashboardView/index.js
@@ -1,14 +1,35 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-const DashboardView = () => (
-    <>
-        <h1>Dashboard View</h1>
+const FETCH_FLASK_MESSAGE = gql`
+    query FetchFlaskMessage {
+        message @rest(type: "String", path: "/")
+    }
+`;
 
-        <ul>
-            <li><Link to="/campaigns">Campaign List</Link></li>
-        </ul>
-    </>
-);
+const DashboardView = () => {
+    const {loading, error, data} = useQuery(FETCH_FLASK_MESSAGE);
+
+    if (loading) {
+        return <span>Loading...</span>;
+    }
+
+    if (error) {
+        return <span>An error occurred. We could not fetch the Flask API message at <code>/</code></span>;
+    }
+
+    return (
+        <>
+            <h1>Dashboard View</h1>
+            <p>A short message from your Flask API: "{data?.message}"</p>
+
+            <ul>
+                <li><Link to="/campaigns">Campaign List</Link></li>
+            </ul>
+        </>
+    );
+}
 
 export default DashboardView;
