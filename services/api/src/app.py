9f52c35e4cc31c5551fe51cef3719566eb0400c7
Joe Hawkins <joe@aktify.com>
2020-02-26 14:49:46 -0700

    feat[api]: add CORS support to API using Flask-CORS library
    This change was made to enable development and should not be deployed as-is to any production environments.

--- services/api/src/app.py
+++ services/api/src/app.py
@@ -1,7 +1,11 @@
 from flask import Flask
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+# DO NOT deploy this application to a production environment without first adjusting
+# the CORS settings
+CORS(app)
 
 
 @app.route('/')
